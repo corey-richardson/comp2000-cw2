ae751cfd57f24017bf7c862fcac8f2f478cfbc56
Corey Richardson <richardsoncorey04@gmail.com>
2024-12-17 18:03:37 +0000

    Created PtoRequest class

--- SourceCode/app/src/main/java/com/example/employeeapp/PtoRequest.java
+++ SourceCode/app/src/main/java/com/example/employeeapp/PtoRequest.java
@@ -1,4 +1,53 @@
 package com.example.employeeapp;
 
+import java.util.Date;
+import android.util.Log;
+
 public class PtoRequest {
+    private int id;
+    private int requester_id;
+    private int approver_id;
+    private String status;
+    private Date start_date;
+    private Date end_date;
+    private String request_comment;
+
+    public PtoRequest(int id, int requester_id, int approver_id, String status, Date start_date,
+                      Date end_date, String request_comment) {
+        this.id = id;
+        this.requester_id = requester_id;
+        this.approver_id = approver_id;
+        this.status = status;
+        this.start_date = start_date;
+        this.end_date = end_date;
+        this.request_comment = request_comment;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getRequester_id() {
+        return requester_id;
+    }
+
+    public int getApprover_id() {
+        return approver_id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public Date getStart_date() {
+        return start_date;
+    }
+
+    public Date getEnd_date() {
+        return end_date;
+    }
+
+    public String getRequest_comment() {
+        return request_comment;
+    }
 }
