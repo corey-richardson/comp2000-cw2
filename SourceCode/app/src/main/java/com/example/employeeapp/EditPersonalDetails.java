a4b9b4d4153fb9ce61de5a30d982e00db9c6cf0b
Corey Richardson <richardsoncorey04@gmail.com>
2024-12-19 18:13:58 +0000

    Pre-populate edit details fields with current information

--- SourceCode/app/src/main/java/com/example/employeeapp/EditPersonalDetails.java
+++ SourceCode/app/src/main/java/com/example/employeeapp/EditPersonalDetails.java
@@ -1,7 +1,12 @@
 package com.example.employeeapp;
 
 import android.os.Bundle;
-
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -9,6 +14,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class EditPersonalDetails extends AppCompatActivity {
+
+    Employee currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,23 @@ public class EditPersonalDetails extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        currentUser = DatabaseHelper.loadCurrentUser(this);
+
+        TextView employeeNameTextView = findViewById(R.id.employeeName);
+        employeeNameTextView.setText(currentUser.getFull_name());
+
+        Date startDate = currentUser.getStart_date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        String startDateString = dateFormat.format(startDate);
+
+        ((TextView) findViewById(R.id.employeeDetailsFirstNameField)).setText(currentUser.getFirst_name());
+        ((TextView) findViewById(R.id.employeeDetailsLastNameField)).setText(currentUser.getLast_name());
+        ((TextView) findViewById(R.id.employeeDetailsEmailField)).setText(currentUser.getEmail());
+        ((TextView) findViewById(R.id.employeeDetailsPhoneField)).setText(currentUser.getPhone());
+        ((TextView) findViewById(R.id.employeeDetailsAddressField)).setText(currentUser.getAddress());
+        ((TextView) findViewById(R.id.employeeDetailsJobTitleField)).setText(currentUser.getJob_title());
+        ((EditText) findViewById(R.id.employeeDetailsStartDateField)).setText(startDateString);
     }
 }
