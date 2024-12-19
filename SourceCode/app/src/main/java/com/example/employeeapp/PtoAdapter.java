1846c70fec99925eb127bc551698eea0eeb21792
Corey Richardson <richardsoncorey04@gmail.com>
2024-12-19 22:42:06 +0000

    Use a Custom View to display PTO Requests and their status

--- SourceCode/app/src/main/java/com/example/employeeapp/PtoAdapter.java
+++ SourceCode/app/src/main/java/com/example/employeeapp/PtoAdapter.java
@@ -5,8 +5,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.TextView;
-
+import android.graphics.Color;
+import android.graphics.Typeface;
 import java.util.List;
 
 public class PtoAdapter extends BaseAdapter {
@@ -37,16 +39,41 @@ public class PtoAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         if (convertView == null) {
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = inflater.inflate(android.R.layout.simple_list_item_2, null);
+            convertView = inflater.inflate(R.layout.pto_list_item, null);
         }
 
         PtoRequest ptoRequest = ptoRequestList.get(position);
 
-        TextView text1 = convertView.findViewById(android.R.id.text1);
-        TextView text2 = convertView.findViewById(android.R.id.text2);
+        String ptoDateRange = context.getString(R.string.pto_request_dates, ptoRequest.getStart_date(), ptoRequest.getEnd_date());
+        TextView ptoDateRangeTextField = convertView.findViewById(R.id.pto_start_end_date);
+        TextView ptoStatusTextField = convertView.findViewById(R.id.pto_status);
 
-        text1.setText(ptoRequest.getStart_date() + " | " + ptoRequest.getEnd_date());
-        text2.setText(ptoRequest.getStatus());
+        Button cancelButton = convertView.findViewById(R.id.cancel_button);
+        Button editButton = convertView.findViewById(R.id.edit_button);
+        Button resubmitButton = convertView.findViewById(R.id.resubmit_button);
+
+        ptoDateRangeTextField.setText(ptoDateRange);
+        ptoStatusTextField.setText(ptoRequest.getStatus());
+
+        switch (ptoRequest.getStatus()) {
+            case "Approved":
+                ptoStatusTextField.setTextColor(Color.parseColor("#06402B"));
+                ptoStatusTextField.setTypeface(null, Typeface.BOLD);
+                cancelButton.setVisibility(View.GONE);
+                editButton.setVisibility(View.GONE);
+                resubmitButton.setVisibility(View.GONE);
+                break;
+            case "Denied":
+                ptoStatusTextField.setTextColor(Color.parseColor("#FF0000"));
+                ptoStatusTextField.setTypeface(null, Typeface.BOLD);
+                cancelButton.setVisibility(View.GONE);
+                editButton.setVisibility(View.GONE);
+                resubmitButton.setVisibility(View.VISIBLE);
+                break;
+            default:
+                resubmitButton.setVisibility(View.GONE);
+                break;
+        }
 
         return convertView;
     }
