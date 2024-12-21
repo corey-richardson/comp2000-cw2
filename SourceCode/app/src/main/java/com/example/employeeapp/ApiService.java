4cb7b9a35c1bff1349f786c7ec4a03fe75c2529a
Corey Richardson <richardsoncorey04@gmail.com>
2024-12-21 23:50:25 +0000

    Fetch `Employee`s from API and store locally
    Only if they follow my schema! Other people are ruining the API and it should've been separated per student!!!

--- SourceCode/app/src/main/java/com/example/employeeapp/ApiService.java
+++ SourceCode/app/src/main/java/com/example/employeeapp/ApiService.java
@@ -1,6 +1,7 @@
 package com.example.employeeapp;
 
 import android.content.Context;
+import android.database.SQLException;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -8,12 +9,14 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.HashMap;
@@ -107,6 +110,71 @@ public class ApiService {
 
                 queue = getRequestQueue(context);
                 queue.add(request);
+            }
+        });
+    }
+
+    public static void fetchAndStoreEmployees(Context context) {
+        String fetchEmployeesUrl = API_URL + "/employees";
+        DatabaseHelper databaseHelper = DatabaseHelper.getInstance(context);
+
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                        Request.Method.GET, fetchEmployeesUrl, null,
+                        new Response.Listener<JSONArray>() {
+                            @Override
+                            public void onResponse(JSONArray response) {
+                                for (int i = 0; i < response.length(); i++) {
+                                    try {
+                                        JSONObject jsonObject = response.getJSONObject(i);
+                                        if (databaseHelper.getEmployeeById(jsonObject.getInt("id")) != null) { continue; } // Employee already in local database
+                                        Log.d("Employee", jsonObject.getString("firstname") + jsonObject.getString("lastname"));
+
+                                        try {
+                                            Employee employee = new Employee(
+                                                    jsonObject.getInt("id"),
+                                                    jsonObject.getString("firstname"),
+                                                    jsonObject.getString("lastname"),
+                                                    jsonObject.getString("email"),
+                                                    jsonObject.getString("department"),
+                                                    (float) jsonObject.getDouble("salary"),
+                                                    jsonObject.getString("joiningdate"),
+                                                    jsonObject.getInt("leaves"),
+                                                    "employee_password",
+                                                    "Employee"
+                                            );
+
+                                            try {
+                                                databaseHelper.insertUser(employee);
+                                            } catch (SQLException e) {
+                                                Log.e("Constraint", "Employee doesn't follow schema rules.");
+                                            }
+
+
+                                        } catch (Exception e) {
+                                            Log.e("CreateEmployee", "Couldn't parse fetched employee to Employee Object.");
+                                        }
+
+                                    } catch (Exception e) {
+                                        Log.e("ParseError", "Error parsing JSON Object from API.");
+                                    }
+                                }
+                            }
+
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.e("FetchEmployees", "Error fetching employees: " + error.getMessage());
+                                Toast.makeText(context, "Failed to fetch employees!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                );
+
+                queue = getRequestQueue(context);
+                queue.add(jsonArrayRequest);
             }
         });
     }
